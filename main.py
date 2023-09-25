15a426a35898f6424e835c693e2e49594e584c0c
gvielza <80008338+gvielza@users.noreply.github.com>
2023-09-25 20:56:27 +0000

    add form

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from persona import Persona
 import sqlite3
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-  return 'Hello from Flask! Tks'
+  if request.method=='POST':
+    nombre=request.form.get('nombre')
+    apellido=request.form.get('apellido')
+    edad=request.form.get('edad')
+    persona=Persona(nombre,apellido,edad)
+    return f' la persona{persona.nombre} se guardo correctamente'
+
+  
+  return render_template('formulario.html')
 
 
 app.run(host='0.0.0.0', port=81)
+if __name__ == '__main__':
+    app.run(debug=True)
